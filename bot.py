79f44569f8de1595bd3f08a4ccf078a64fd44e4f
Bryan <bryan@ASi-Bryan-Vernons-Macbook.local>
2023-10-12 12:59:07 -0400

    Non Webhook Method
    Tried a new method of running

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from slack_sdk import WebClient
-from godaddypy import Client, Account
+import requests
 import ssl
 from dotenv import load_dotenv
 
 ssl._create_default_https_context = ssl._create_unverified_context
-
 load_dotenv()
+
 
 app = Flask(__name__)
 
@@ -19,38 +19,56 @@ channel_id = '#' + channel_id
 godaddy_api_key = os.environ.get('GODADDY_API_KEY')
 godaddy_api_secret = os.environ.get('GODADDY_API_SECRET')
 
-account = Account(api_key=godaddy_api_key, api_secret=godaddy_api_secret)
-client = Client(account)
-
-@app.route('/webhook/godaddy', methods=['POST'])
-def godaddy_webhook():
+@app.route('/nameservers_change', methods=['POST'])
+def nameservers_change():
     data = request.json
+    domain_name = data['domain_name']
+    old_nameservers = data['old_nameservers']
+    new_nameservers = data['new_nameservers']
+    message = f"Nameservers changed for {domain_name}:\nOld Nameservers: {', '.join(old_nameservers)}\nNew Nameservers: {', '.join(new_nameservers)}"
 
-    # Check if the event is about a domain transfer
-    if data['event'] == 'domain_transfer_in':
-        domain_name = data['domain_name']
-        message = f"Domain transferred in: {domain_name}"
-
-    elif data['event'] == 'domain_transfer_out':
-        domain_name = data['domain_name']
-        message = f"Domain transferred out: {domain_name}"
-
-    elif data['event'] == 'nameserver_change':
-        domain_name = data['domain_name']
-        old_nameservers = data['old_nameservers']
-        new_nameservers = data['new_nameservers']
-        message = f"Nameservers changed for {domain_name}:\nOld Nameservers: {', '.join(old_nameservers)}\nNew Nameservers: {', '.join(new_nameservers)}"
-
-        # Send the nameserver change notification to Slack
-        slack_client.chat_postMessage(channel=channel_id, text=message)
-
-    else:
-        message = "Unknown event received from GoDaddy"
+    slack_client.chat_postMessage(channel=channel_id, text=message)
 
     return 'OK', 200
 
+@app.route('/transfer_in', methods=['POST'])
+def transfer_in():
+    data = request.json
+    domain_name = data['domain_name']
+    message = f"Domain transferred in: {domain_name}"
+
+    slack_client.chat_postMessage(channel=channel_id, text=message)
+
+    return 'OK', 200
+
+@app.route('/transfer_out', methods=['POST'])
+def transfer_out():
+    data = request.json
+    domain_name = data['domain_name']
+    message = f"Domain transferred out: {domain_name}"
+
+    slack_client.chat_postMessage(channel=channel_id, text=message)
+
+    return 'OK', 200
+
+@app.route('/renewal', methods=['POST'])
+def domain_renewal():
+    data = request.json
+
+    if data['event'] == 'domain_renewal':
+        domain_name = data['domain_name']
+        renewal_period = data['renewal_period']  # Optionally, you can get the renewal period
+
+        message = f"Domain renewal for {domain_name}. Renewal period: {renewal_period} years."
+
+        slack_client.chat_postMessage(channel=channel_id, text=message)
+
+        return 'OK', 200
+
+    return 'Not a domain renewal event', 200
+
 if __name__ == '__main__':
-    startup_message = "Notifications for Godaddy has started running."
+    startup_message = "Notifications for Godaddy have started running."
     slack_client.chat_postMessage(channel=channel_id, text=startup_message)
 
     app.run(host='0.0.0.0', port=8000)
